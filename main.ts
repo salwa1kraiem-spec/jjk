e5dec924bb4b11f70e78fe00b0d4e0ca9e7f1480
salwa1kraiem-spec <salwa1.kraiem@gmail.com>
2026-02-22 22:01:40 +0100

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,26 @@
 input.onButtonPressed(Button.A, function () {
-	
+    priere += 1
+    if (priere == 100) {
+        pins.digitalWritePin(DigitalPin.P0, 1)
+    }
+})
+input.onButtonPressed(Button.AB, function () {
+    if (input.compassHeading() >= 90) {
+        basic.showLeds(`
+            . . # . .
+            . . . # .
+            # # # # #
+            . . . # .
+            . . # . .
+            `)
+        basic.showString("c'est l'est")
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    control.reset()
+})
+input.onGesture(Gesture.Shake, function () {
+    basic.showNumber(priere)
 })
 let priere = 0
+priere = 0
